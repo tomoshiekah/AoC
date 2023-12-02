312b5a2c967f409c13421758309919f44d5089ee
tomoshiekah <106881927+tomoshiekah@users.noreply.github.com>
2023-12-02 10:26:50 +0100

    day1 & 2

---
+++ day1.py
@@ -0,0 +1,54 @@
+NUMBERS = {
+    "one" : 1, 
+    "two" : 2, 
+    "three" : 3, 
+    "four" : 4, 
+    "five" : 5, 
+    "six" : 6, 
+    "seven" : 7, 
+    "eight" : 8, 
+    "nine" : 9
+}
+
+def main():
+    cValues = []
+
+    with open('day1.txt') as f:
+        for line in f:
+            cValues.append(getCalibrationValue(line.strip()))
+    return sum(cValues)
+
+def getCalibrationValue(input):
+
+    first = findNumberW(input, True)
+    last = findNumberW(input[::-1], False)
+
+    return int(str(first) + str(last))
+
+def findNumber(input):
+    for char in input:
+        if char.isdigit():
+            return char
+        
+            
+def findNumberW(input, first):
+    for i in range(len(input)):
+        if input[i].isdigit():
+            return input[i]
+        else:
+            writtenN = checkIfNumber(input, i, first)
+            if writtenN is not None:
+                return writtenN
+
+def checkIfNumber(input, index, first):
+    for written in NUMBERS.keys():
+        if not first:
+            written = written[::-1]
+
+        if input[index: index+len(written)] == written:
+            if not first:
+                return NUMBERS[written[::-1]]
+            return NUMBERS[written]
+        
+
+print(main())
---
+++ day2.py
@@ -0,0 +1,62 @@
+AMOUNT = {
+    "red" : 12,
+    "green" : 13,
+    "blue" : 14
+}
+
+def part1():
+    gameNumbers = []
+
+    with open('day2.txt') as f:
+        for line in f:
+            game, input = line.strip().split(":")
+            if isPossible(input):
+                gameNumbers.append(int(game.split(" ")[1]))
+            
+    return sum(gameNumbers)
+
+def isPossible(input):
+    rounds = input.split(";")
+    for round in rounds:
+        colors = round.split(",")
+        for color in colors:
+            amount, name = color.strip().split(" ")
+            if int(amount) > AMOUNT[name]:
+                return False
+    
+    return True
+
+def part2():
+    gameNumbers = []
+
+    with open('day2.txt') as f:
+        for line in f:
+            game, input = line.strip().split(":")
+            minAmounts = getMinAmounts(input)
+            power = 1
+            for a in minAmounts:
+                power = power * minAmounts[a]
+            
+            gameNumbers.append(power)
+            
+    return sum(gameNumbers)
+
+def getMinAmounts(input):
+    minAmounts = {
+        "red" : 0,
+        "green" : 0,
+        "blue" : 0
+    }
+
+    rounds = input.split(";")
+    for round in rounds:
+        colors = round.split(",")
+        for color in colors:
+            amount, name = color.strip().split(" ")
+            if minAmounts[name] < int(amount):
+                minAmounts[name] = int(amount)
+    
+    return minAmounts
+
+
+print(part2())
